db273e984a43ab967865f8aab5ef70e4ee9d288d
Natannaels <109231090+Natannaels@users.noreply.github.com>
2024-05-29 10:10:41 -0300

    Update App.java

--- ReinoUFMA/src/App.java
+++ ReinoUFMA/src/App.java
@@ -1,9 +1,8 @@
 public class App {
     public static void main(String[] args) {
-       Personagem mago=new Mago("Cabeçudinho", 10, 10);
-       Personagem guer = new Guerreiro("Jurandir", 10, 10);
-       Personagem arq = new Arqueiro("Hella", 10, 10);
-        mago.setNivel(2);
+       Personagem mago=new Mago("Valdemart", 10, 10);
+       Personagem guer = new Guerreiro("Aquiles", 10, 10);
+       Personagem arq = new Arqueiro("Merida", 10, 10);
        Personagem.combate(mago, arq);
 
     }
